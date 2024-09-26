3a12a1a9ce16afbda540f51ffbec8a80a4f0c76d
Zeni-Tech <zainalicheema9@gmail.com>
2024-09-26 09:26:54 -0700

    Password Generator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PasswordGenerator from './PasswordGenerator/PasswordGenerator';
+
+function App() {
+  return (
+    <>
+    <PasswordGenerator/>
+    </>
+  );
+}
+
+export default App;
